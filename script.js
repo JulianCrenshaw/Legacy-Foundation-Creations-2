a99a7c042e2bfe5f7727c3227b2693831f7a4f69
Julian <julian.gold.crenshaw@gmail.com>
2026-04-17 07:41:13 -0500

    update resize

--- script.js
+++ script.js
@@ -12,3 +12,20 @@ function offers() {
 const offers= document.getElementById("ourOffers")
 offers.innerHTML =  "Every day is a new beginning! Don't let the opportunities pass you by! <br>go out, meet new people, and enjoy life!"
 }
+
+ function handleResize() {
+    const links = document.querySelectorAll(".page__link");
+
+    if (window.innerWidth <= 480) {
+      links.forEach(link => {
+        link.onclick = (e) => e.preventDefault();
+      });
+    } else {
+      links.forEach(link => {
+        link.onclick = null;
+      });
+    }
+  }
+
+  window.addEventListener("resize", handleResize);
+  handleResize();
